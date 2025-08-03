0b46c208feae6cbce15703f1c0d0872e1500529e
xlow <lowxf7@gmail.com>
2025-08-04 01:33:00 +0800

    feat: Command execute() throws CustomException, implemented AddCommand

---
+++ CET2012_P02_Low_Xian_Feng/src/command/AddCommand.java
@@ -0,0 +1,24 @@
+package command;
+import core.CustomException;
+import core.EmployeeRegistry;
+
+public class AddCommand implements UndoableCommand {
+    private final EmployeeRegistry registry;
+    private final String[] data;
+
+    public AddCommand(EmployeeRegistry registry,
+                      String data1, String data2, String data3) {
+        this.registry = registry;
+        this.data = new String[]{data1, data2, data3};
+    }
+
+    @Override
+    public void execute() throws CustomException {
+        registry.add(data);
+    }
+
+    @Override
+    public void undo() {
+        registry.deleteLastEntry();
+    }
+}
--- CET2012_P02_Low_Xian_Feng/src/command/Command.java
+++ CET2012_P02_Low_Xian_Feng/src/command/Command.java
@@ -1,5 +1,7 @@
 package command;
 
+import core.CustomException;
+
 public interface Command {
-    void execute();
+    void execute() throws CustomException;
 }
