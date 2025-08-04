143c630d01b2089cfd1f2e9d2e2b348cbedfce21
xlow <lowxf7@gmail.com>
2025-08-04 22:54:50 +0800

    feat: implemented invoker class

---
+++ CET2012_P02_Low_Xian_Feng/src/core/Invoker.java
@@ -0,0 +1,22 @@
+package core;
+
+import command.Command;
+import java.util.Stack;
+
+public class Invoker {
+	private Command[] cmdToExecute;
+
+	public void setCommandsForExecution(Command[] cmds) {
+		this.cmdToExecute = cmds;
+	}
+
+	public void executeCommand(Stack<Command> history) {
+		for (Command cmd: cmdToExecute) {
+			try {
+				cmd.execute(history);
+			} catch (CustomException e) {
+				System.out.println(e.getMessage());
+			}
+		}
+	}
+}
