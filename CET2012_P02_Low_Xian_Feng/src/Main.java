cf4bf7f3a25ea57aa12e30db06ba37b63855b6e9
x-low <lowxf7@gmail.com>
2025-08-05 15:15:31 +0800

    testing: new Main using Manager and Invoker

--- CET2012_P02_Low_Xian_Feng/src/Main.java
+++ CET2012_P02_Low_Xian_Feng/src/Main.java
@@ -1,15 +1,32 @@
 import command.*;
 import core.*;
 
+import java.util.Stack;
+
 public class Main {
+
     public static void main(String[] args) {
         EmployeeRegistry registry;
+        EmployeeManager manager;
+        Invoker invoker = new Invoker();
+        Stack<Command> history = new Stack<Command>();
         try {
             registry = new EmployeeRegistry();
         } catch (CustomException e) {
             System.out.println(e.getMessage());
             return ;
         }
+        manager = new EmployeeManager(registry);
+        Command[] payload = new Command[]{new AddCommand(manager, "first", "first", "email"),
+                new AddCommand(manager, "Second", "sEcOnD", "email"),
+                new AddCommand(manager, "THIRD", "ThIRd", "email"),
+                new ListCommand(manager),
+        };
+
+        invoker.setCommandsForExecution(payload);
+        invoker.executeCommand(history);
+        System.out.println(history.toString());
+
         try {
             registry.storeToFile();
         } catch (CustomException e) {
