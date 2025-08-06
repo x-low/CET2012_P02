b4ef1c5128adebfd3a7136f1e53e2b48c5001745
x-low <lowxf7@gmail.com>
2025-08-06 15:09:21 +0800

    testing: email test cases

--- CET2012_P02_Low_Xian_Feng/src/core/ManagerTools.java
+++ CET2012_P02_Low_Xian_Feng/src/core/ManagerTools.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 
 public class ManagerTools {
     private static final Pattern emailPolicy =
-            Pattern.compile("^[^.\\-](?![.\\-]{2})[\\w.\\-]*(?<![.\\-])@[a-zA-Z.\\-]+(?<![.\\-])(\\.[a-z]{2,3})$");
+            Pattern.compile("^[^.\\-](?!.*[.]{2})(?!.*[-]{2})[\\w.\\-]*(?<![.\\-])@[a-zA-Z0-9][a-zA-Z0-9.\\-]*(?<![.\\-])(\\.[a-z]{2,3})$");
     private static final Pattern latinEmail =
             Pattern.compile("^\\w+$");
 
