657254f6b788bd9129424ca4f7c68729a8fd9a57
xlow <lowxf7@gmail.com>
2025-08-08 00:57:58 +0800

    docs: Javadoc for AddCommand

--- CET2012_P02_Low_Xian_Feng/src/command/AddCommand.java
+++ CET2012_P02_Low_Xian_Feng/src/command/AddCommand.java
@@ -3,15 +3,36 @@ package command;
 import core.CustomException;
 import core.EmployeeManager;
 
+/**
+ * Accepts a {@code String} to be entered as a record into the datastore
+ * following the pattern of "first_name last_name email"
+ */
 public class AddCommand implements Command {
+    /**
+     * Receiver responsible for handling and storing data
+     */
     private final EmployeeManager manager;
+    /**
+     * Input split into datafields by spaces
+     */
     private final String[] data;
 
+    /**
+     * Constructs an {@code AddCommand} with the receiver and entry to add.
+     * Splits input into separate data fields
+     * @param employeeManager data storage handler
+     * @param input entry to be added
+     */
     public AddCommand(EmployeeManager employeeManager, String input) {
         this.manager = employeeManager;
         this.data = input.split(" ");
     }
 
+    /**
+     * Calls datastore handler to execute the add operation
+     * @throws CustomException if number of data fields is not 3 or
+     * the operation fails in the datastore handler
+     */
     @Override
     public void execute() throws CustomException {
         if (data.length != 3)
@@ -20,11 +41,21 @@ public class AddCommand implements Command {
         System.out.println("add");
     }
 
+    /**
+     * Undo the most recent add operation by removing the last entry
+     * in the datastore
+     * @throws CustomException if there is no entry to be removed
+     */
     @Override
     public void undo() throws CustomException {
         manager.deleteLastEntry();
     }
 
+    /**
+     * Returns {@code true} indicating undo functionality
+     * is not supported
+     * @return {@code true}
+     */
     @Override
     public boolean canUndo() {
         return true;
