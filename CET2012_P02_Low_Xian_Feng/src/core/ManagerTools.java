cf4a098329bb0c523a09cad8a8a10a6e1be9c2d9
xlow <lowxf7@gmail.com>
2025-08-08 00:22:33 +0800

    fix: emailPolicy no longer allows '.-' or '-.' pattern

--- CET2012_P02_Low_Xian_Feng/src/core/ManagerTools.java
+++ CET2012_P02_Low_Xian_Feng/src/core/ManagerTools.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 
 public class ManagerTools {
     private static final Pattern emailPolicy =
-            Pattern.compile("^\\w(?!.*[.]{2})(?!.*[-]{2})[\\w.\\-]*" +
+            Pattern.compile("^\\w(?!.*[.\\-]{2})[\\w.\\-]*" +
                     "(?<![.\\-])@[a-zA-Z0-9][a-zA-Z0-9.\\-]*" +
                     "(?<![.\\-])(\\.[a-z]{2,3})$");
     private static final Pattern latinEmail =
