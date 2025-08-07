5209ce4e580e3d42542b50575e29c0848d994428
xlow <lowxf7@gmail.com>
2025-08-08 00:43:38 +0800

    docs: Javadoc for Command interface

--- CET2012_P02_Low_Xian_Feng/src/command/Command.java
+++ CET2012_P02_Low_Xian_Feng/src/command/Command.java
@@ -2,19 +2,30 @@ package command;
 
 import core.CustomException;
 
-import java.util.Stack;
-
 /**
- * An object representing an executable command with
- * method for indicating whether it supports undo functionality
+ * Represents an executable action capable of executing a command,
+ * to check whether it support an undo functionality and if it does,
+ * the ability to undo a previously executed command
  */
 public interface Command {
     /**
-     * Executes the command's functionality through it's included
-     * receiver. Prints name of command upon successful execution
-     * @throws CustomException if wrong number of arguments
+     * Executes corresponding command's main action
+     * @throws CustomException if input is invalid or action
+     * could not be successfully carried out by receiver
      */
     void execute() throws CustomException;
+
+    /**
+     * Undo the effects of previously executed command
+     * if undo is supported
+     * @throws CustomException if undo is not supported or action
+     * could not be successfully carried out by receiver
+     */
     void undo() throws CustomException;
+
+    /**
+     * Indicates whether command supports undo functionality
+     * @return {@code true} if undo is supported, {@code false} if not
+     */
     boolean canUndo();
 }
