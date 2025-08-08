a89b4cd870701e87bc987e8b16bbe3ad51651e5c
x-low <lowxf7@gmail.com>
2025-08-08 13:19:10 +0800

    docs: Javadoc for ManagerTools

--- CET2012_P02_Low_Xian_Feng/src/core/ManagerTools.java
+++ CET2012_P02_Low_Xian_Feng/src/core/ManagerTools.java
@@ -3,14 +3,35 @@ package core;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * Contains helper methods utilised by {@code EmployeeManager}
+ */
 public class ManagerTools {
+    /**
+     * Pattern enforcing email policy only matching valid emails
+     */
     private static final Pattern emailPolicy =
             Pattern.compile("^\\w(?!.*[.\\-]{2})[\\w.\\-]*" +
                     "(?<![.\\-])@[a-zA-Z0-9][a-zA-Z0-9.\\-]*" +
                     "(?<![.\\-])(\\.[a-z]{2,3})$");
+    /**
+     * Pattern enforcing input only matching latin letters and underscores
+     * for if typical email format is not followed
+     */
     private static final Pattern latinEmail =
             Pattern.compile("^\\w+$");
 
+    /**
+     * {@code ManagerTools} is non-instantiable
+     * as it only contains static methods
+     */
+    private ManagerTools() {}
+
+    /**
+     * Converts {@code String} input to title case
+     * @param str {@code String} to be made title case
+     * @return {@code String} input in title case
+     */
     public static String toTitleCase(String str) {
         StringBuilder sb = new StringBuilder();
         boolean nextUpperCase = true;
