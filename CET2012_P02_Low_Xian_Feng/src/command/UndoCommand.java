1ae29515b7eddcba52e06d4aa48d42ebedb1a2fd
x-low <lowxf7@gmail.com>
2025-08-08 12:38:53 +0800

    docs: Javadoc for UndoCommand

--- CET2012_P02_Low_Xian_Feng/src/command/UndoCommand.java
+++ CET2012_P02_Low_Xian_Feng/src/command/UndoCommand.java
@@ -4,15 +4,36 @@ import core.CustomException;
 import core.EmployeeManager;
 import java.util.Stack;
 
+/**
+ * Represents a request to undo the previously executed command
+ */
 public class UndoCommand implements Command {
+    /**
+     * Receiver responsible for handling and storing data
+     */
     private final EmployeeManager manager;
+    /**
+     * Stores previously executed commands that support undo
+     */
     private final Stack<Command> history;
 
+    /**
+     * Constructs a {@code UndoCommand} with the receiver and command history
+     * @param employeeManager data storage handler
+     * @param commandHistory {@code Stack} containing undoable previously
+     *                       executed commands
+     */
     public UndoCommand(EmployeeManager employeeManager, Stack<Command> commandHistory) {
         this.manager = employeeManager;
         this.history = commandHistory;
     }
 
+    /**
+     * Calls the undo function of the previously executed
+     * command, if any
+     * @throws CustomException if there are no previously executed commands
+     * or if {@code undo()} of previously executed command throws
+     */
     @Override
     public void execute() throws CustomException {
         if (history.isEmpty())
@@ -22,11 +43,19 @@ public class UndoCommand implements Command {
         System.out.println("undo");
     }
 
+    /**
+     * Undo is not supported for the undo command
+     * @throws CustomException as undo functionality is not supported
+     */
     @Override
     public void undo() throws CustomException {
         throw new CustomException("Error: Cannot undo");
     }
 
+    /**
+     * Returns {@code false} indicating undo functionality is not supported
+     * @return {@code false}
+     */
     @Override
     public boolean canUndo() {
         return false;
