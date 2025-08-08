5c148a10a8509b9d8ff66e6831609e5bc243f95e
x-low <lowxf7@gmail.com>
2025-08-08 12:41:01 +0800

    docs: Javadoc for CustomException

--- CET2012_P02_Low_Xian_Feng/src/core/CustomException.java
+++ CET2012_P02_Low_Xian_Feng/src/core/CustomException.java
@@ -1,6 +1,14 @@
 package core;
 
+/**
+ * Exception class exclusively thrown by errors encountered by
+ * EmployeeManager and Command classes
+ */
 public class CustomException extends Exception {
+    /**
+     * Constructs custom exception message for printing
+     * @param message the error message
+     */
     public CustomException(String message) {
         super(message);
     }
