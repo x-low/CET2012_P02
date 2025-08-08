0864f9234b10932523e2fe2364005cb045e4d08d
x-low <lowxf7@gmail.com>
2025-08-08 13:05:29 +0800

    docs: Javadoc for FileHandler

--- CET2012_P02_Low_Xian_Feng/src/core/FileHandler.java
+++ CET2012_P02_Low_Xian_Feng/src/core/FileHandler.java
@@ -6,11 +6,25 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 
+/**
+ * Handles reading from, writing to and storing contents of datastore file
+ */
 public class FileHandler {
+    /**
+     * Content of datastore file
+     */
     private ArrayList<String> dataStore;
+    /**
+     * Path to datastore file
+     */
     private static final String dataStorePath =
             System.getProperty("user.dir") + "/src/dataStore.txt";
 
+    /**
+     * Constructs a {@code FileHandler}, reading and storing datastore
+     * file contents or creating an empty datastore if file does not
+     * exist or cannot be read
+     */
     public FileHandler() {
         Path data = Paths.get(dataStorePath);
 
@@ -26,6 +40,10 @@ public class FileHandler {
         }
     }
 
+    /**
+     * Saves contents of datastore back into the file after
+     * modifying commands have been executed
+     */
     public void storeToFile() {
         Path data = Paths.get(dataStorePath);
 
@@ -36,6 +54,10 @@ public class FileHandler {
         }
     }
 
+    /**
+     * Returns contents of datastore
+     * @return @{code ArrayList} datastore
+     */
     ArrayList<String> getDataStore() {
         return (this.dataStore);
     }
